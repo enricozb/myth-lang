9c90b2c68a85cf55499dbbbd7aaa37c68ac98b38
Enrico Borba <enricozb@gmail.com>
2017-08-27 16:44:17 -0700

    Added support for operators, 3 shift/reduce.

--- myth.py
+++ myth.py
@@ -1,20 +1,21 @@
-import readline
-
 tokens = (
     'NAME', 'NUMBER', 'COLON', 'LPAREN', 'RPAREN', 'LBRACKET', 'RBRACKET',
-    'COMMA', 'MAPSTO'
+    'LBRACE', 'RBRACE', 'COMMA', 'MAPSTO', 'RPAREN_MAPSTO',
 )
 
 t_ignore = ' '
 
-t_NAME = r'[a-zA-Z_]+'
+t_NAME = r'[a-zA-Z_]+|[\+\-\/\*\^]+|[\.]'
 t_COLON = r':'
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
 t_LBRACKET = r'\['
 t_RBRACKET = r'\]'
+t_LBRACE = r'{'
+t_RBRACE = r'}'
 t_COMMA = r','
 t_MAPSTO = r'->'
+t_RPAREN_MAPSTO = r'\)\s+->'
 
 def t_NUMBER(t):
     r'\d+'
@@ -42,6 +43,9 @@ class DictStack:
     def pop(self):
         self.dicts.pop()
 
+    def update(self, d):
+        self.dicts[-1].update(d)
+
     def __getitem__(self, key):
         for d in self.dicts[::-1]:
             if key in d:
@@ -51,7 +55,11 @@ class DictStack:
     def __setitem__(self, key, val):
         self.dicts[-1][key] = val
 
+from functools import reduce, partial
 names = DictStack(locals()['__builtins__'].__dict__)
+names.update({'reduce': reduce, 'partial': partial})
+names['+'] = lambda x, y: x + y
+names['.'] = lambda f, g: lambda x: f(g(x))
 
 def lambda_literal(capturelist, expression):
     def func(*args):
@@ -71,6 +79,8 @@ def eval_bytecode(code):
     elif instr == 'call':
         return eval_bytecode(args[0])(*map(eval_bytecode, args[1]))
     elif instr == 'literal':
+        if type(args[0]) in (list, set):
+            return type(args[0])(map(eval_bytecode, args[0]))
         return args[0]
     elif instr == 'name':
         return names[args[0]]
@@ -121,7 +131,8 @@ def p_expression_name(t):
 
 def p_expression(t):
     """
-    expression : call
+    expression : operator_invocation
+               | call
                | literal
     """
     t[0] = t[1]
@@ -130,9 +141,20 @@ def p_literal_number(t):
     """
     literal : NUMBER
             | list
+            | set
             | lambda
     """
     t[0] = ('literal', t[1])
+
+def p_set(t):
+    """
+    set : LBRACE arglist RBRACE
+        | LBRACE RBRACKET
+    """
+    if len(t) == 3:
+        t[0] = set()
+    else:
+        t[0] = set(t[2])
 
 def p_list(t):
     """
@@ -142,25 +164,39 @@ def p_list(t):
     if len(t) == 3:
         t[0] = []
     else:
-        t[0] = list(map(eval_bytecode, t[2]))
+        t[0] = t[2]
 
-def p_lambda_singlearg(t):
+def p_lambda(t):
     """
     lambda : NAME MAPSTO expression
-           | LPAREN NAME RPAREN MAPSTO expression
+           | LPAREN capture_list RPAREN_MAPSTO expression
     """
     if len(t) == 4:
         t[0] = lambda_literal([t[1]], t[3])
     else:
-        t[0] = lambda_literal([t[2]], t[5])
+        t[0] = lambda_literal(t[2], t[4])
 
-def p_lambda(t):
+def p_capture_list(t):
     """
-    lambda : LPAREN arglist RPAREN MAPSTO expression
+    capture_list : NAME
+                 | capture_list COMMA NAME
     """
-    if not all(map(lambda x: x[0] == 'name', t[2])):
-        raise SyntaxError('Wat')
-    t[0] = lambda_literal([name for _, name in t[2]], t[5])
+    if len(t) == 2:
+        t[0] = [t[1]]
+    else:
+        t[0] = t[1] + [t[3]]
+
+def p_operator(t):
+    """
+    operator : NAME
+    """
+    t[0] = ('name', t[1])
+
+def p_operator_invocation(t):
+    """
+    operator_invocation : expression operator expression
+    """
+    t[0] = ('call', t[2], [t[1], t[3]])
 
 
 def p_error(t):
@@ -170,10 +206,22 @@ def p_error(t):
 import ply.yacc as yacc
 parser = yacc.yacc()
 
+import readline
+import os
+
+historypath = os.path.expanduser('~/.mythhistory')
+
+if os.path.exists(historypath):
+    readline.read_history_file(historypath)
+else:
+    open(historypath, 'w').close()
+
 while True:
     try:
         s = input('myth> ')
-        yacc.parse(s)
+        if s:
+            readline.append_history_file(1, historypath)
+            yacc.parse(s)
     except EOFError:
         break
     except Exception as e:
--- parsetab.py
+++ parsetab.py
@@ -5,9 +5,9 @@ _tabversion = '3.8'
 
 _lr_method = 'LALR'
 
-_lr_signature = '5A71933CDB48B29F5F59D712E6D05AAC'
+_lr_signature = 'CDF7E00E8EFA487A864E0CEE31D4A929'
     
-_lr_action_items = {'NAME':([0,7,11,12,13,14,27,31,32,34,],[4,17,21,21,21,21,21,21,21,21,]),'LPAREN':([0,4,7,11,12,13,14,17,21,25,27,31,32,34,],[7,13,7,7,7,7,7,13,13,31,7,7,7,7,]),'NUMBER':([0,7,11,12,13,14,27,31,32,34,],[8,8,8,8,8,8,8,8,8,8,]),'LBRACKET':([0,7,11,12,13,14,27,31,32,34,],[11,11,11,11,11,11,11,11,11,11,]),'$end':([1,2,3,4,5,6,8,9,10,19,21,22,24,29,30,36,37,38,],[0,-1,-2,-8,-9,-10,-11,-12,-13,-15,-8,-7,-16,-14,-5,-18,-17,-6,]),'COLON':([4,],[12,]),'MAPSTO':([4,17,21,26,28,],[14,14,14,32,34,]),'RPAREN':([5,6,8,9,10,15,16,17,19,20,21,23,24,29,30,33,35,36,37,38,],[-9,-10,-11,-12,-13,25,26,28,-15,-3,-8,30,-16,-14,-5,-4,38,-18,-17,-6,]),'COMMA':([5,6,8,9,10,15,16,17,18,19,20,21,23,24,29,30,33,35,36,37,38,],[-9,-10,-11,-12,-13,-3,27,-8,27,-15,-3,-8,27,-16,-14,-5,-4,27,-18,-17,-6,]),'RBRACKET':([5,6,8,9,10,11,18,19,20,21,24,29,30,33,36,37,38,],[-9,-10,-11,-12,-13,19,29,-15,-3,-8,-16,-14,-5,-4,-18,-17,-6,]),}
+_lr_action_items = {'NAME':([0,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,22,24,25,26,28,29,30,32,34,35,36,37,38,39,40,41,43,45,],[4,16,-8,-9,-10,-11,22,-12,-13,-14,-15,26,26,26,-24,26,26,26,16,-8,-19,16,-8,-17,16,16,16,26,42,-18,26,-16,-5,26,16,16,-6,]),'LPAREN':([0,4,8,13,14,15,16,17,18,19,22,26,33,34,37,40,],[8,18,8,8,8,8,-24,8,8,8,18,18,40,8,8,8,]),'NUMBER':([0,8,13,14,15,16,17,18,19,34,37,40,],[9,9,9,9,9,-24,9,9,9,9,9,9,]),'LBRACKET':([0,8,13,14,15,16,17,18,19,34,37,40,],[13,13,13,13,13,-24,13,13,13,13,13,13,]),'LBRACE':([0,8,13,14,15,16,17,18,19,34,37,40,],[14,14,14,14,14,-24,14,14,14,14,14,14,]),'$end':([1,2,3,4,5,6,7,9,10,11,12,24,26,28,29,30,32,36,38,39,41,45,],[0,-1,-2,-8,-9,-10,-11,-12,-13,-14,-15,-19,-8,-17,-25,-7,-20,-18,-16,-5,-21,-6,]),'COLON':([4,],[17,]),'MAPSTO':([4,22,26,],[19,19,19,]),'RPAREN':([5,6,7,9,10,11,12,20,22,24,25,26,28,29,31,32,36,38,39,41,43,44,45,],[-9,-10,-11,-12,-13,-14,-15,33,-8,-19,-3,-8,-17,-25,39,-20,-18,-16,-5,-21,-4,45,-6,]),'RBRACKET':([5,6,7,9,10,11,12,13,14,23,24,25,26,28,29,32,36,38,39,41,43,45,],[-9,-10,-11,-12,-13,-14,-15,24,28,36,-19,-3,-8,-17,-25,-20,-18,-16,-5,-21,-4,-6,]),'COMMA':([5,6,7,9,10,11,12,21,22,23,24,25,26,27,28,29,31,32,36,38,39,41,42,43,44,45,],[-9,-10,-11,-12,-13,-14,-15,35,-22,37,-19,-3,-8,37,-17,-25,37,-20,-18,-16,-5,-21,-23,-4,37,-6,]),'RBRACE':([5,6,7,9,10,11,12,24,25,26,27,28,29,32,36,38,39,41,43,45,],[-9,-10,-11,-12,-13,-14,-15,-19,-3,-8,38,-17,-25,-20,-18,-16,-5,-21,-4,-6,]),'RPAREN_MAPSTO':([21,22,42,],[34,-22,-23,]),}
 
 _lr_action = {}
 for _k, _v in _lr_action_items.items():
@@ -16,7 +16,7 @@ for _k, _v in _lr_action_items.items():
       _lr_action[_x][_k] = _y
 del _lr_action_items
 
-_lr_goto_items = {'statement':([0,],[1,]),'assign':([0,],[2,]),'expression':([0,7,11,12,13,14,27,31,32,34,],[3,15,20,22,20,24,33,20,36,37,]),'call':([0,7,11,12,13,14,27,31,32,34,],[5,5,5,5,5,5,5,5,5,5,]),'literal':([0,7,11,12,13,14,27,31,32,34,],[6,6,6,6,6,6,6,6,6,6,]),'list':([0,7,11,12,13,14,27,31,32,34,],[9,9,9,9,9,9,9,9,9,9,]),'lambda':([0,7,11,12,13,14,27,31,32,34,],[10,10,10,10,10,10,10,10,10,10,]),'arglist':([7,11,13,31,],[16,18,23,35,]),}
+_lr_goto_items = {'statement':([0,],[1,]),'assign':([0,],[2,]),'expression':([0,8,13,14,15,17,18,19,34,37,40,],[3,20,25,25,29,30,25,32,41,43,25,]),'operator_invocation':([0,8,13,14,15,17,18,19,34,37,40,],[5,5,5,5,5,5,5,5,5,5,5,]),'call':([0,8,13,14,15,17,18,19,34,37,40,],[6,6,6,6,6,6,6,6,6,6,6,]),'literal':([0,8,13,14,15,17,18,19,34,37,40,],[7,7,7,7,7,7,7,7,7,7,7,]),'list':([0,8,13,14,15,17,18,19,34,37,40,],[10,10,10,10,10,10,10,10,10,10,10,]),'set':([0,8,13,14,15,17,18,19,34,37,40,],[11,11,11,11,11,11,11,11,11,11,11,]),'lambda':([0,8,13,14,15,17,18,19,34,37,40,],[12,12,12,12,12,12,12,12,12,12,12,]),'operator':([3,20,25,29,30,32,41,43,],[15,15,15,15,15,15,15,15,]),'capture_list':([8,],[21,]),'arglist':([13,14,18,40,],[23,27,31,44,]),}
 
 _lr_goto = {}
 for _k, _v in _lr_goto_items.items():
@@ -26,22 +26,29 @@ for _k, _v in _lr_goto_items.items():
 del _lr_goto_items
 _lr_productions = [
   ("S' -> statement","S'",1,None,None,None),
-  ('statement -> assign','statement',1,'p_statment','myth.py',80),
-  ('statement -> expression','statement',1,'p_statment','myth.py',81),
-  ('arglist -> expression','arglist',1,'p_arglist','myth.py',89),
-  ('arglist -> arglist COMMA expression','arglist',3,'p_arglist','myth.py',90),
-  ('call -> NAME LPAREN arglist RPAREN','call',4,'p_call_name','myth.py',99),
-  ('call -> LPAREN expression RPAREN LPAREN arglist RPAREN','call',6,'p_call_expression','myth.py',105),
-  ('assign -> NAME COLON expression','assign',3,'p_assign','myth.py',111),
-  ('expression -> NAME','expression',1,'p_expression_name','myth.py',117),
-  ('expression -> call','expression',1,'p_expression','myth.py',123),
-  ('expression -> literal','expression',1,'p_expression','myth.py',124),
-  ('literal -> NUMBER','literal',1,'p_literal_number','myth.py',130),
-  ('literal -> list','literal',1,'p_literal_number','myth.py',131),
-  ('literal -> lambda','literal',1,'p_literal_number','myth.py',132),
-  ('list -> LBRACKET arglist RBRACKET','list',3,'p_list','myth.py',138),
-  ('list -> LBRACKET RBRACKET','list',2,'p_list','myth.py',139),
-  ('lambda -> NAME MAPSTO expression','lambda',3,'p_lambda_singlearg','myth.py',148),
-  ('lambda -> LPAREN NAME RPAREN MAPSTO expression','lambda',5,'p_lambda_singlearg','myth.py',149),
-  ('lambda -> LPAREN arglist RPAREN MAPSTO expression','lambda',5,'p_lambda','myth.py',158),
+  ('statement -> assign','statement',1,'p_statment','myth.py',90),
+  ('statement -> expression','statement',1,'p_statment','myth.py',91),
+  ('arglist -> expression','arglist',1,'p_arglist','myth.py',100),
+  ('arglist -> arglist COMMA expression','arglist',3,'p_arglist','myth.py',101),
+  ('call -> NAME LPAREN arglist RPAREN','call',4,'p_call_name','myth.py',110),
+  ('call -> LPAREN expression RPAREN LPAREN arglist RPAREN','call',6,'p_call_expression','myth.py',116),
+  ('assign -> NAME COLON expression','assign',3,'p_assign','myth.py',122),
+  ('expression -> NAME','expression',1,'p_expression_name','myth.py',128),
+  ('expression -> operator_invocation','expression',1,'p_expression','myth.py',134),
+  ('expression -> call','expression',1,'p_expression','myth.py',135),
+  ('expression -> literal','expression',1,'p_expression','myth.py',136),
+  ('literal -> NUMBER','literal',1,'p_literal_number','myth.py',142),
+  ('literal -> list','literal',1,'p_literal_number','myth.py',143),
+  ('literal -> set','literal',1,'p_literal_number','myth.py',144),
+  ('literal -> lambda','literal',1,'p_literal_number','myth.py',145),
+  ('set -> LBRACE arglist RBRACE','set',3,'p_set','myth.py',151),
+  ('set -> LBRACE RBRACKET','set',2,'p_set','myth.py',152),
+  ('list -> LBRACKET arglist RBRACKET','list',3,'p_list','myth.py',161),
+  ('list -> LBRACKET RBRACKET','list',2,'p_list','myth.py',162),
+  ('lambda -> NAME MAPSTO expression','lambda',3,'p_lambda','myth.py',171),
+  ('lambda -> LPAREN capture_list RPAREN_MAPSTO expression','lambda',4,'p_lambda','myth.py',172),
+  ('capture_list -> NAME','capture_list',1,'p_capture_list','myth.py',181),
+  ('capture_list -> capture_list COMMA NAME','capture_list',3,'p_capture_list','myth.py',182),
+  ('operator -> NAME','operator',1,'p_operator','myth.py',191),
+  ('operator_invocation -> expression operator expression','operator_invocation',3,'p_operator_invocation','myth.py',197),
 ]
